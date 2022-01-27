07c575817949014d9652804e146fc004ce74022d
Michał Hankus <michal83h@gmail.com>
2022-01-27 11:08:14 +0100

    workspace-fixes (#6)

--- examples/data-sources/buddy_webhook/data-source.tf
+++ examples/data-sources/buddy_webhook/data-source.tf
@@ -1,7 +1,7 @@
-data "buddy_workspace" "by_name" {
-  name = "Test"
+data "buddy_webhook" "by_id" {
+  webhook_id = 123456
 }
 
-data "buddy_workspace" "by_domain" {
-  domain = "test"
+data "buddy_webhook" "by_target_url" {
+  target_url = "https://mydomain.com"
 }
