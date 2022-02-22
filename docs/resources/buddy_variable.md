b7e8acd105732cde8164f46c512665f314890d0e
Michal <michal83h@gmail.com>
2022-02-22 12:42:16 +0100

    more examples

--- docs/resources/buddy_variable.md
+++ docs/resources/buddy_variable.md
@@ -20,9 +20,27 @@ Token scopes required: `WORKSPACE`, `VARIABLE_ADD`, `VARIABLE_MANAGE`, `VARIABLE
 
 ```terraform
 resource "buddy_variable" "mysecret" {
-  domain = "mydomain"
-  key    = "ACCESS_KEY"
-  value  = "abcdefgh"
+  domain    = "mydomain"
+  key       = "ACCESS_KEY"
+  value     = "abcdefgh"
+  encrypted = true
+}
+
+resource "buddy_variable" "settable_in_project" {
+  domain       = "mydomain"
+  project_name = "myproject"
+  key          = "KEY"
+  value        = "init"
+  settable     = true
+}
+
+resource "buddy_variable" "in_pipeline" {
+  domain       = "mydomain"
+  project_name = "myproject"
+  pipeline_id  = 123456
+  key          = "KEY"
+  value        = "VAL"
+  description  = "variable visibile only in this pipeline"
 }
 ```
 
--- examples/resources/buddy_pipeline/resource.tf
+++ examples/resources/buddy_pipeline/resource.tf
@@ -1,7 +1,84 @@
-resource "buddy_pipeline" "test" {
+resource "buddy_pipeline" "click" {
+  domain              = "mydomain"
+  project_name        = "myproject"
+  name                = "click"
+  on                  = "CLICK"
+  refs                = ["main"]
+  always_from_scratch = true
+}
+
+resource "buddy_pipeline" "event" {
+  domain         = "mydomain"
+  project_name   = "myproject"
+  name           = "event"
+  on             = "EVENT"
+  priority       = "HIGH"
+  fetch_all_refs = true
+
+  event {
+    type = "PUSH"
+    refs = ["master"]
+  }
+}
+
+resource "buddy_pipeline" "schedule" {
+  domain                      = "mydomain"
+  project_name                = "myproject"
+  name                        = "schedule"
+  on                          = "SCHEDULE"
+  priority                    = "LOW"
+  fail_on_prepare_env_warning = true
+  start_date                  = "2016-11-18T12:38:16.000Z"
+  delay                       = 10
+  paused                      = true
+}
+
+resource "buddy_pipeline" "schedule_cron" {
   domain       = "mydomain"
   project_name = "myproject"
-  name         = "test"
+  name         = "schedule_cron"
+  on           = "SCHEDULE"
+  cron         = "15 14 1 * *"
+}
+
+resource "buddy_pipeline" "conditions" {
+  domain       = "mydomain"
+  project_name = "myproject"
+  name         = "conditions"
   on           = "CLICK"
   refs         = ["main"]
+
+  trigger_condition {
+    condition = "ON_CHANGE"
+  }
+  trigger_condition {
+    condition = "ON_CHANGE_AT_PATH"
+    paths     = ["/abc"]
+  }
+  trigger_condition {
+    condition      = "VAR_IS"
+    variable_key   = "KEY"
+    variable_value = "VAL"
+  }
+  trigger_condition {
+    condition      = "VAR_IS_NOT"
+    variable_key   = "KEY"
+    variable_value = "VAL"
+  }
+  trigger_condition {
+    condition      = "VAR_CONTAINS"
+    variable_key   = "KEY"
+    variable_value = "VAL"
+  }
+  trigger_condition {
+    condition      = "VAR_NOT_CONTAINS"
+    variable_key   = "KEY"
+    variable_value = "VAL"
+  }
+  trigger_condition {
+    condition = "DATETIME"
+    hours     = [10]
+    days      = [1, 20]
+    zone_id   = "America/Monterrey"
+  }
 }
